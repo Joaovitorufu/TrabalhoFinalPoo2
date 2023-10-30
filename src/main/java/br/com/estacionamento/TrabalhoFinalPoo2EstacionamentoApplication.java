bc008fb2962d99aed7235698b308d01ad5630eba
Joaovitorufu <joaovitorufu@gmail.com>
2023-10-30 19:52:33 -0300

    First commit

---
+++ src/main/java/br/com/estacionamento/TrabalhoFinalPoo2EstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.estacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalPoo2EstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalPoo2EstacionamentoApplication.class, args);
+	}
+
+}
