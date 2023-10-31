963f67c56d3a13bf284d7385f97648924d81da30
Joaovitorufu <joaovitorufu@gmail.com>
2023-10-31 19:19:48 -0300

    estrutura de pastas inicial

--- src/main/java/br/com/estacionamento/controllers/HelloWorldController.java
+++ src/main/java/br/com/estacionamento/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package br.com.estacionamento;
+package br.com.estacionamento.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
