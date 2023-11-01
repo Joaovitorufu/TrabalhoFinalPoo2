fd100f3df114c46b985325aef17ceb0dc30f6f02
Joaovitorufu <joaovitorufu@gmail.com>
2023-10-31 21:58:33 -0300

    add lista de veiculos

--- src/main/java/br/com/estacionamento/entities/Estacionamento.java
+++ src/main/java/br/com/estacionamento/entities/Estacionamento.java
@@ -1,7 +1,10 @@
 package br.com.estacionamento.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class Estacionamento {
-    List<Veiculo> veiculosEstacionados;
+    List<Veiculo> veiculosEstacionados = new ArrayList<Veiculo>();
 }
