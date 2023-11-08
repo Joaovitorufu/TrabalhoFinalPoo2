0bcb1022267817a5a0232ba3c2de2741e001e7d8
Joaovitorufu <joaovitorufu@gmail.com>
2023-11-07 23:07:46 -0300

    arrumando merge

--- src/Motocicleta.java
+++ src/Motocicleta.java
@@ -1,5 +1,13 @@
 public class Motocicleta extends Veiculo {
+
     public Motocicleta(String cor, String modelo, String placa, Integer idVaga, PessoaTemplate proprietario) {
         super(cor, modelo, placa, idVaga, proprietario);
+
+
+    }
+
+    @Override
+    public void accept(Visitor v) {
+        v.visit(this);
     }
 }
